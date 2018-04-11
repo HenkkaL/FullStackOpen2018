13893a9781b218ef8c7b66f3d402493b398ee6cc
HenkkaL <henriklyra@gmail.com>
2018-04-11 20:15:28 +0300

    tehtava 1.6

---
+++ unicafe/src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            hyva: 0,
+            neutraali: 0,
+            huono: 0,
+        }
+    }
+    
+    kasvataHyva = () => {
+        this.setState({ hyva: this.state.hyva + 1 })
+    }
+
+    kasvataNeutraali = () => {
+        this.setState({ neutraali: this.state.neutraali + 1 })
+    }
+
+    kasvataHuono = () => {
+        this.setState({ huono: this.state.huono + 1 })
+    }
+
+    render() {
+        const keskiarvo = this.state.hyva
+        return (
+            <div>
+                <div>
+                    <h1>anna palautetta</h1>
+                    <button onClick={ this.kasvataHyva } >Hyvä</button>
+                    <button onClick={ this.kasvataNeutraali } >Neutraali</button>
+                    <button onClick={ this.kasvataHuono } >Huono</button>                    
+                </div>
+                <div>
+                    <h1>statistiikka</h1>   
+                    <p>Hyvä: {this.state.hyva }</p>
+                    <p>Neutraali: {this.state.neutraali }</p>
+                    <p>Huono: {this.state.huono }</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root'))
