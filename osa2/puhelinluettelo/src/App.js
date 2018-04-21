cb0eb5dd6207675a8207576d82c42d4c14804857
Henrik Lyra <henrik.lyra@stat.fi>
2018-04-21 10:21:53 +0300

    tehtava 2.6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,21 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+const Contact = ({contact}) => {
+  return (
+    <li>{contact.name}</li>
+  )
+}
+
+const Contacts = ({contacts}) => {
+  return (
+    <ul>
+      {contacts.map(item => <Contact key={item.name} contact={item} />)}
+    </ul>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addContact = (event) => {
+    event.preventDefault()
+    const contact = {
+      name: this.state.newName
+    }   
+
+    const persons = this.state.persons.concat(contact)
+
+    this.setState({
+      persons,
+      newName:''
+    })
+  }
+
+  handleNameChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ newName: event.target.value })
+    
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addContact}>
+          <div>
+            nimi: <input
+             value={this.state.newName}
+             onChange={this.handleNameChange}
+             />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+          <Contacts contacts={this.state.persons} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
